c24352fcb8b74afc6612df9c49dd6641af5166ae
Eder Afonso Rabelo <eafonsorabelo@gmail.com>
2025-06-11 12:32:27 -0400

    Initial commit

---
+++ src/main/java/com/ederrabelo/challenge_modelodominio_orm/ChallengeModelodominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.ederrabelo.challenge_modelodominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeModelodominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeModelodominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ederrabelo/challenge_modelodominio_orm/ChallengeModelodominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederrabelo.challenge_modelodominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeModelodominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
